d56ec541f4ffe68364fb4c01e0def3f34609949e
Thanh Pham <thanhpham091790@gmail.com>
2022-06-14 13:48:48 -0600

    Test connect with css, js files. Run build comand and deploy project to Netlify server.

---
+++ dist/index.eccc68ac.js
@@ -0,0 +1,2 @@
+const heading=document.querySelector("h1");heading.addEventListener("click",(()=>{alert("Check JS!")}));
+//# sourceMappingURL=index.eccc68ac.js.map
---
+++ src/js/scripts.js
@@ -0,0 +1,4 @@
+const heading = document.querySelector('h1');
+heading.addEventListener('click', () => {
+    alert('Check JS!');
+});
