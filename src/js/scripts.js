66b1de74442868e00140bbef2b70e115f971d536
Thanh Pham <thanhpham091790@gmail.com>
2022-06-16 17:16:23 -0600

    Breakpoint < 576px: Styling the skills section.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,4 @@
+
 const menu_icon = document.querySelector('#menu');
 const close_icon = document.querySelector('#close');
 const mobile_nav = document.querySelector('#drop-down');
@@ -21,7 +22,7 @@ const gridContainer = document.querySelector('#grid-container');
 const columns = gridContainer.querySelectorAll('.column');
 
 function porfolioFilter(id) {
-    // Add active class to filter button when button was clicked.
+    // Add 'active' class to filter button when button was clicked.
     for (let i = 0; i < btnList.length; i++) {
         btnList[i].classList.remove('active');
         if (btnList[i].id == id) {
@@ -29,29 +30,38 @@ function porfolioFilter(id) {
         }
     }
 
+    // Add 'show' class to all column when 'all' button was clicked.
     if (id == 'all') {
         for (let i = 0; i < columns.length; i++) {
             if (!columns[i].classList.contains(id)) {
                 columns[i].classList.add('show');
             }
-            // console.log(columns[i].classList);
         }
-    } else {
+    } else { // Otherwise, only add 'show' class to responding column.
         for (let i = 0; i < columns.length; i++) {
             columns[i].classList.remove('show');
             if (columns[i].classList.contains(id)) {
                 columns[i].classList.add('show');
             }
-            // console.log(columns[i].classList);
         }
     }
 
 }
 
-
+// Attach 'click' event to all filter buttons.
 for (let i = 0; i < btnList.length; i++) {
     let btnId = btnList[i].id;
     btnList[i].addEventListener('click', () => {
         porfolioFilter(btnId);
     });
+}
+
+
+const percentList = document.querySelectorAll('.percent');
+for (let i = 0; i < percentList.length; i++) {
+    let text = percentList[i].textContent;
+    let percentIndex = text.indexOf('%');
+    let num = parseInt(text.slice(0, percentIndex));
+    console.log(num);
+    percentList[i].style.width = `${num}%`;
 }
