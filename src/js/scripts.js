300c98cadd9100a411523fd034706277109b69ef
Thanh Pham <thanhpham091790@gmail.com>
2022-06-23 17:30:54 -0600

    Add Tiny edit text to project.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -39,6 +39,17 @@ const map = L.mapquest.map('map', {
 });
 /* End: Add some javascript for map. */
 
+/* Start: Add tiny textarea */
+tinymce.init({
+    selector: 'textarea',
+    plugins: 'a11ychecker advcode casechange export formatpainter image editimage linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tableofcontents tinycomments tinymcespellchecker',
+    toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter image editimage pageembed permanentpen table tableofcontents',
+    toolbar_mode: 'floating',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+});
+/* End: Add tiny textarea */
+
 
 /* Start: Use fetch to retrieve the projects. */
 
